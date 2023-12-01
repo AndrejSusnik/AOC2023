c245824703359bf9dd5a8dc793697a9768f8e1a9
Andrej Susnik <susnik.andrej7@gmail.com>
2023-12-01 12:42:28 +0100

    Improve solution for day1

--- Day01/001.py
+++ Day01/001.py
@@ -1,34 +1,19 @@
+import re
 solution1 = sum(map(lambda x: int(x[0] + x[-1]),[list(filter(lambda x: x.isnumeric(), line.strip())) for line in open('input1').readlines()]))
 print(solution1)
 
-
-str_to_digit = {
-    "one": '1',
-    "two": '2',
-    "three": '3',
-    "four": '4',
-    "five": '5',
-    "six": '6',
-    "seven": '7',
-    "eight": '8',
-    "nine": '9',
-}
-
-
 def str_to_int(input: str) -> list[int]:
-    numbers = []
-    for i in range(len(input)):
-        if input[i].isnumeric():
-            numbers.append(input[i])
-            continue
+    input = re.sub('one', 'o1e', input)
+    input = re.sub('two', 't2o', input)
+    input = re.sub('three', 't3e', input)
+    input = re.sub('four', 'f4r', input)
+    input = re.sub('five', 'f5e', input)
+    input = re.sub('six', 's6x', input)
+    input = re.sub('seven', 's7n', input)
+    input = re.sub('eight', 'e8t', input)
+    input = re.sub('nine', 'n9e', input)
 
-        for key, value in str_to_digit.items():
-            if i + len(key) <= len(input) and input[i: i + len(key)] == key:
-                numbers.append(value)
-                break
-
-    return numbers
-
+    return list(filter(lambda x: x.isnumeric(), input))
 
 solution2 = sum(map(lambda x: int(x[0] + x[-1]),[str_to_int(line.strip()) for line in open('input1').readlines()]))
 print(solution2)
