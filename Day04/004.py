b1136edd48c09546c14dbc553f70d02964d3b171
Andrej Susnik <susnik.andrej7@gmail.com>
2023-12-04 14:26:11 +0100

    Cleanup solution for day 4

--- Day04/004.py
+++ Day04/004.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-scores = list(map(lambda x: len(np.intersect1d(x[0], x[1])), [list(map(lambda x: np.array(list(map(int, x.split()))), line.strip().split(':')[1].split('|'))) for line in open('input').readlines()]))
+if __name__ == "__main__":
+    scores = list(map(lambda x: len(np.intersect1d(x[0], x[1])),
+                      [list(map(lambda x: np.array(list(map(int, x.split()))),
+                                line.strip().split(':')[1].split('|')))
+                      for line in open('input').readlines()]))
 
-print(sum(map(lambda x: 2 ** (x - 1) if x > 0 else 0, scores)))
+    print(sum(map(lambda x: 2 ** (x - 1) if x > 0 else 0, scores)))
 
+    scrds = np.ones(len(scores))
 
-scrds = np.ones(len(scores))
+    for i, value in enumerate(scores):
+        scrds[i + 1: i+value+1] += 1 * scrds[i]
 
-for i, value in enumerate(scores):
-    scrds[i + 1: i+value+1] += 1 * scrds[i]
-
-print(int(sum(scrds)))
-    
-
+    print(int(sum(scrds)))
