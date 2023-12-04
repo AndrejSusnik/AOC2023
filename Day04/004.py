69e5c44c6c46ed5c6778155b22e4747bca951b3e
Andrej Susnik <susnik.andrej7@gmail.com>
2023-12-04 12:54:33 +0100

    Add solution for day 4

---
+++ Day04/004.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+scores = list(map(lambda x: len(np.intersect1d(x[0], x[1])), [list(map(lambda x: np.array(list(map(int, x.split()))), line.strip().split(':')[1].split('|'))) for line in open('input').readlines()]))
+
+print(sum(map(lambda x: 2 ** (x - 1) if x > 0 else 0, scores)))
+
+
+scrds = np.ones(len(scores))
+
+for i, value in enumerate(scores):
+    scrds[i + 1: i+value+1] += 1 * scrds[i]
+
+print(int(sum(scrds)))
+    
+
